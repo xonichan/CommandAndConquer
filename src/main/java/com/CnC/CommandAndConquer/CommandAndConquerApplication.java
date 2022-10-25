037b116f6aa3aea9e3edd96161319eb634f130fe
Nicholas <30299411+xonichan@users.noreply.github.com>
2022-10-25 22:32:59 +0300

    fix logs

--- src/main/java/com/CnC/CommandAndConquer/CommandAndConquerApplication.java
+++ src/main/java/com/CnC/CommandAndConquer/CommandAndConquerApplication.java
@@ -1,13 +1,18 @@
 package com.CnC.CommandAndConquer;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommandAndConquerApplication {
 
+	static final Logger logger =
+			LoggerFactory.getLogger(CommandAndConquerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CommandAndConquerApplication.class, args);
+		logger.info("Starting CommandAndConquerApplication.");
 	}
 
 }
--- src/main/java/com/CnC/CommandAndConquer/controller/CnCController.java
+++ src/main/java/com/CnC/CommandAndConquer/controller/CnCController.java
@@ -1,7 +1,7 @@
 package com.CnC.CommandAndConquer.controller;
+import com.CnC.CommandAndConquer.CommandAndConquerApplication;
 import com.CnC.CommandAndConquer.entiry.CnCEntity;
 import com.CnC.CommandAndConquer.service.CnCService;
-import org.apache.logging.log4j.LogManager;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
+@SuppressWarnings("unused")
 @RestController
 public class CnCController {
+    static final Logger logger = LoggerFactory.getLogger(CommandAndConquerApplication.class);
 
-    Logger log = LogManager.getLogger();
 
     @Autowired
     CnCService cnCService;
 
     @RequestMapping(value = "GetCity", method = RequestMethod.GET)
     public List<CnCEntity> getItem(@RequestParam("cityName") @NotNull String cityName) {
-        log.info("Search for the city of " + cityName);
+        logger.info("Search for the city of " + cityName);
         return cnCService.findByNameCity(cityName);
     }
 
@@ -35,7 +38,7 @@ public class CnCController {
     @RequestMapping(value = "SaveCityName", method = RequestMethod.GET)
     public String saveCityName(@RequestParam("cityName") @NotNull String cityName) {
         cnCService.saveEntity(cityName);
-        log.info("Save city " + cityName);
+        logger.info("Save city " + cityName);
         return "Add city " + cityName;
     }
 }
